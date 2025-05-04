5bd5baae815ff4b0d282f7606bc248b20947354a
c8villanueva <caitlin.villanueva@gmail.com>
2025-05-04 21:44:25 +0800

    test deploy

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500', //change in deployment
+    // baseUrl: 'http://localhost:3500', //change in deployment
+    // baseUrl: 'https://technotes-api.onrender.com',
+    baseUrl: 'https://technotes-7wis.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
